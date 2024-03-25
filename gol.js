5c6af65b88e1d2ef9f945acd40c43a2643600a46
airommel <airommel@users.noreply.github.com>
2024-03-25 19:18:52 +0800

    code review

--- gol.js
+++ gol.js
@@ -11,16 +11,29 @@ let reproduction_threshold = 3;
 let slider;
 let color = 150;
 let currentPoint = [0, 0];
-
+// currentPoint = {x:0,y:0}
+let speciesColorScheme = {
+    A: "yellow",
+    B: "black",
+    Z: "blue"
+}
+function randomMachine() {
+    let dice = random()
+    if (dice < 0.3) {
+        return { species: "A", living: 1 }
+    } else if (dice >= 0.3) {
+        return { species: "B", living: 1 }
+    }
+}
 
 window.addEventListener("load", () => {
 
     let closeButtons = document.querySelectorAll(".close");
 
     for (let i = 0; i < closeButtons.length; i++) {
-      closeButtons[i].addEventListener("click", function() {
-        document.querySelector(".popup").style.display = "none";
-      });
+        closeButtons[i].addEventListener("click", function () {
+            document.querySelector(".popup").style.display = "none";
+        });
     }
 
     document.querySelector(".slidecontainer").addEventListener("change", (e) => {
@@ -30,17 +43,23 @@ window.addEventListener("load", () => {
     document.querySelector(".colorPickerContainer").addEventListener("input", (e) => {
         boxColor = e.target.value
     })
+
+
     document.querySelector("#buttonRandom").addEventListener("click", (e) => {
+        console.log("random")
         for (let i = 0; i < columns; i++) {
             for (let j = 0; j < rows; j++) {
-                currentBoard[i][j] = random() > 0.8 ? 1 : 0;
+
+                currentBoard[i][j] = randomMachine();
                 nextBoard[i][j] = 0;
             }
-        } redraw();
+        }
+        redraw();
     })
 
 
     document.querySelector("#buttonPlay").addEventListener("click", (e) => {
+
         loop()//play the game
     })
 
@@ -70,11 +89,50 @@ window.addEventListener("load", () => {
     let sliderValue = document.getElementById("myRange");
     let output = document.getElementById("demo");
     output.innerHTML = sliderValue.value;
-    sliderValue.oninput = function() {
-    output.innerHTML = this.value;
-      }
+    sliderValue.oninput = function () {
+        output.innerHTML = this.value;
+    }
 
 })
+
+
+
+const gliderPattern = `.O
+..O
+OOO`
+
+function insertPattern(patternInput) {
+    if (mouseX > unitLength * columns || mouseY > unitLength * rows) {
+        return;
+    }
+
+    const x = Math.floor(mouseX / unitLength);
+    const y = Math.floor(mouseY / unitLength);
+
+    redraw()
+    let parsedArray = patternInput.split('\n')
+    // console.log(parsedArray)
+
+    for (let j in parsedArray) {
+        // console.log(j, parsedArray[j])
+
+        for (let i in parsedArray[j]) {
+            // console.log("value of cell at i:", i, "j:", j, ":", parsedArray[j][i])
+            if (parsedArray[j][i] == "O") {
+                // currentBoard[x+i][y+j] = 1
+                // console.log("insert pattern")
+                fill(58, 64, 160);
+                stroke(strokeColor);
+
+                // console.log(x + Number(i), y + Number(j))
+                rect((x + Number(i)) * unitLength, (y + Number(j)) * unitLength, unitLength, unitLength);
+            }
+        }
+    }
+
+}
+
+insertPattern(gliderPattern)
 
 
 function setup() {
@@ -118,11 +176,22 @@ function windowResized() {
 function init() {
     for (let i = 0; i < columns; i++) {
         for (let j = 0; j < rows; j++) {
-            currentBoard[i][j] = 0;
-            nextBoard[i][j] = 0;
+            currentBoard[i][j] = { species: "Z", living: 0 };
+            nextBoard[i][j] = { species: "Z", living: 0 };
         }
     }
-    currentBoard[currentPoint[0]][currentPoint[1]] = 2
+    currentPoint = [floor(columns / 2), floor(rows / 2)]
+    currentBoard[currentPoint[0]][currentPoint[1]].preview = true
+
+
+    currentBoard[1][0] = { species: "A", living: 1 }
+    currentBoard[1][1] = { species: "A", living: 1 }
+    currentBoard[1][2] = { species: "A", living: 1 }
+
+
+    currentBoard[1][7] = { species: "B", living: 1 }
+    currentBoard[1][8] = { species: "B", living: 1 }
+    currentBoard[1][9] = { species: "B", living: 1 }
     noLoop()
 }
 
@@ -132,14 +201,25 @@ function draw() {
     generate();
     for (let i = 0; i < columns; i++) {
         for (let j = 0; j < rows; j++) {
-            if (currentBoard[i][j] == 1) {
-                if (nextBoard[i][j] == 1) {
-                    fill(tinycolor(boxColor).darken(40).toString());
-                } else
-                    fill(boxColor);
-            } else if (currentBoard[i][j] == 2) {
+
+            // console.log(currentBoard[i][j].living)
+            // console.log((currentBoard[i][j].species), speciesColorScheme[(currentBoard[i][j].species)] )
+            if (currentBoard[i][j].living == 1) {
+                // if (nextBoard[i][j].living == 1) {
+                //     // stable life in this cell
+                //     // console.log("1")
+                //     fill(tinycolor(speciesColorScheme[(currentBoard[i][j].species)]).darken(40).toString());
+                // } else
+                //  life in this cell
+                console.log("2", (currentBoard[i][j].species))
+                fill(speciesColorScheme[(currentBoard[i][j].species)]);
+            } else if (currentBoard[i][j].preview == true) {
+                // keyboard highlight in this cell
+                // console.log("3")
                 fill(58, 64, 160);
             } else {
+                // console.log("4")
+                // nothing in this cell
                 fill(133, 157, 204);
             }
 
@@ -158,7 +238,7 @@ function colorPick() {
 
 
 function keyPressed() {
-    console.log(keyCode)
+
     if (keyCode === LEFT_ARROW) {
         currentBoard[currentPoint[0]][currentPoint[1]] = 0;
         let x = currentPoint[0] === 0 ? columns - 1 : currentPoint[0] - 1;
@@ -177,16 +257,17 @@ function keyPressed() {
         currentBoard[currentPoint[0]][currentPoint[1]] = 0;
         let y = currentPoint[1] === rows - 1 ? 0 : currentPoint[1] + 1;
         currentPoint = [currentPoint[0], y];
-    } 
-    currentBoard[currentPoint[0]][currentPoint[1]] = 2;
+    }
+    currentBoard[currentPoint[0]][currentPoint[1]].preview = true;
     loop();
 }
+
 function generate() {
     //Loop over every single box on the board
     for (let x = 0; x < columns; x++) {
         for (let y = 0; y < rows; y++) {
             // Count all living members in the Moore neighborhood(8 boxes surrounding)
-            let neighbors = 0;
+            let neighbors = { aNeighCount: 0, bNeighCount: 0 };
             for (let i of [-1, 0, 1]) {
                 for (let j of [-1, 0, 1]) {
                     if (i == 0 && j == 0) {
@@ -194,22 +275,36 @@ function generate() {
                         continue;
                     }
                     // The modulo operator is crucial for wrapping on the edge
-                    neighbors +=
-                        currentBoard[(x + i + columns) % columns][(y + j + rows) % rows];
+                    // neighbors +=
+                    //     currentBoard[(x + i + columns) % columns][(y + j + rows) % rows];
+
+                    if ((currentBoard[(x + i + columns) % columns][(y + j + rows) % rows]).living == 1) {
+                        // neighbors++
+                        if ((currentBoard[(x + i + columns) % columns][(y + j + rows) % rows]).species == "A") {
+                            neighbors.aNeighCount++
+                        } else {
+                            neighbors.bNeighCount++
+                        }
+                    }
                 }
             }
 
 
             // Rules of Life
-            if (currentBoard[x][y] == 1 && neighbors < loneliness_threshold) {
+            if (currentBoard[x][y].living == 1 && neighbors.aNeighCount + neighbors.bNeighCount < loneliness_threshold) {
                 // Die of Loneliness
-                nextBoard[x][y] = 0;
-            } else if (currentBoard[x][y] == 1 && neighbors > overpopulation_threshold) {
+                nextBoard[x][y] = { species: "Z", living: 0 };
+            } else if (currentBoard[x][y].living == 1 && neighbors.aNeighCount + neighbors.bNeighCount > overpopulation_threshold) {
                 // Die of Overpopulation
-                nextBoard[x][y] = 0;
-            } else if (currentBoard[x][y] == 0 && neighbors == reproduction_threshold) {
+                nextBoard[x][y] = { species: "Z", living: 0 };
+            } else if (currentBoard[x][y].living == 0 && neighbors.aNeighCount + neighbors.bNeighCount == reproduction_threshold) {
                 // New life due to Reproduction
-                nextBoard[x][y] = 1;
+                if (neighbors.aNeighCount > neighbors.bNeighCount) {
+                    nextBoard[x][y] = { species: "A", living: 1 }
+                } else {
+                    nextBoard[x][y] = { species: "B", living: 1 }
+                }
+
             } else {
                 // Stasis
                 nextBoard[x][y] = currentBoard[x][y];
@@ -217,7 +312,7 @@ function generate() {
         }
     }
 
-   
+
     // Swap the nextBoard to be the current Board
     [currentBoard, nextBoard] = [nextBoard, currentBoard];
 }
@@ -266,4 +361,11 @@ function mouseReleased() {
     }
 
 
+}
+
+
+function mouseMoved() {
+
+
+    insertPattern(gliderPattern)
 }
